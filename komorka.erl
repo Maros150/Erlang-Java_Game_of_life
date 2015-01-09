ae7d5fca1bd23a57e7bd4c87519e103253f9e160
Maros150 <wbachta150@gmail.com>
2015-01-09 17:13:55 +0100

    komentarze

--- komorka.erl
+++ komorka.erl
@@ -2,15 +2,46 @@
 -compile(export_all).
 
 
+
+%-------------------------
+%proces obslugujacy dzialanie komorki
+%o wspolrzednych X i Y
 start(X,Y) ->
-	spawn_link(?MODULE, init, [X,Y,[],niezyje,0]).
-
-init(X,Y,Sasiedzi,Stan,LicznikZyjacych) -> 
-	try run(X,Y,Sasiedzi,Stan,LicznikZyjacych) 
-	catch _ -> io:fwrite("Komorka X , Y umarla")
-	end.
+	spawn_link(?MODULE, init, [X,Y]).
 
 
+%-------------------------
+%wylapywanie wyjatkow i przedwczesnego zakonczenia
+%dzialania komorki
+init(X,Y) -> 
+	try run(X,Y,[],niezyje,0) 
+	catch _ -> io:fwrite("Komorka blad")
+	end,
+	exit(kill).
+
+
+
+
+%------------------------------------------------------
+%glowna funkcja obslugujaca dzialanie komorki
+%Argumenty:
+%	-wspolrzedne X i Y
+%	-lista PID wszystkich 8 sasiadow
+%	-aktualny stan komorki (zyje lub niezyje)
+%	-licznik zyjacych sasiadow
+%
+%{init,InitSasiedzi} - proces nadrzedny wysyla kazdej komorce
+%			 liste jej sasiadow
+%{set,SetStan} - do ozywiania komorek na starcie
+%{ping} - proces nadrzedny zezwala na kolejny krok,
+%		obliczamy nowy stan,
+%		informujemy sasiadow o swoim stanie,
+%		jesli stan komorki sie zmienil informujemy o tym
+%		 program do rysowania
+%		zerujemy licznik zyjacych  						
+%{change,zyje},{change,niezyje} - odebranie wiadomosci			 
+%		z jego aktualnym stanem od sasiada,
+%		jesli zyje zwiekszamy licznik zyjacych sssiadow
 run(X,Y,Sasiedzi,Stan,LicznikZyjacych) ->
 	receive
 		{change,zyje} ->	
@@ -27,26 +58,34 @@ run(X,Y,Sasiedzi,Stan,LicznikZyjacych) ->
 		{init,InitSasiedzi} ->
 			run(X,Y,InitSasiedzi,Stan,LicznikZyjacych);
 
-		{set,SetStan} ->
+		{set,zyje} ->
 			wyslij_sasiadom(zyje, Sasiedzi),
 			rysuj(niezyje,zyje, X, Y),
 			run(X,Y,Sasiedzi,zyje,LicznikZyjacych)	 
 	end.
-	
 
+%------------------------------------------------------	
+%zasady gry 
 sprawdz(zyje,2) ->zyje;
 sprawdz(zyje,3) -> zyje;
 sprawdz(niezyje,3) -> zyje;
 sprawdz(_,_) ->niezyje.  
 
+%------------------------------------------------------
+%funkcja wysylajaca sasiadom komorki wiadomosc z 
+%aktualnym jej stanem
 wyslij_sasiadom(_,[]) -> ok;
 wyslij_sasiadom(Stan,[PID|T]) -> 
 	PID ! {change,Stan},
 	wyslij_sasiadom(Stan,T).
 
+%------------------------------------------------------
+%wyslanie wspolrzednych komorki do programu rysujacego
+%jesli komorka zmienila swoj stan
 rysuj(Stan,NewStan,_,_) when Stan == NewStan -> ok;
 rysuj(_,_,X,Y) ->
 	{echo,java@} ! {X,Y},
 	ok.
 	
 		
+
--- serwer.erl
+++ serwer.erl
@@ -1,46 +1,66 @@
 -module(serwer).
 -compile(export_all).
 
+
+%-------------------------
+%start procesu nadrzednego
 start() -> spawn_link(?MODULE,init,[10,10]).
 
-init(Szer,Wys) -> 
+init(Szer,Wys) ->
 	init(Szer,Wys,stworz_kom(1,1,Szer,Wys,[])).
 
+%-------------------------
+%funkcja wysyla kazdej komorce liste PID jej sasiadow
+%wybiera komorki, ktore maja byc zywe na starcie i
+%startuje gre w zycie
+%Argumenty: rozmiary wyswietlacza(szerokosc i wysokosc)
+%		i lista krotek {X,Y,Pid}-> wspolrzedne i 
+%			Pid komorki
 init(Szer,Wys,Lista) ->
 	init_sasiedzi(Lista,Lista,Szer,Wys),
 	init_poczatkowe_B(Lista),
 	serwerRun(Lista).
 
-
+%-------------------------
+%synchronizacja pracy komorek
 serwerRun(Lista) -> 
 	receive
 		after 2000 -> ping(Lista)
 	end,
 	serwerRun(Lista).
 
+%---------------------------------------------------
 %wyslanie kazdej komorce jej sasiadow(bez brzegowych)
 init_sasiedzi([],_,_,_) -> ok;
 init_sasiedzi([{X,Y,PID}|T],Lista,Szer,Wys) ->
 	PID ! {init,sasiedzi({X,Y,PID},Lista,Szer,Wys)},
 	init_sasiedzi(T,Lista,Szer,Wys).
 
+%---------------------------------------------------
+%zwraca liste Pid sasiadow kazdej komorki
+%nie obsluguje komorek na krawendziach 
 sasiedzi({X,Y,_},Lista,Szer,Wys) when X > 1, Y > 1, X < Szer, Y < Wys -> 
 	[znajdz(X-1,Y-1,Lista), znajdz(X-1,Y,Lista), znajdz(X-1,Y+1,Lista),
 	znajdz(X,Y-1,Lista), znajdz(X,Y+1,Lista),
 	znajdz(X+1,Y-1,Lista), znajdz(X+1,Y,Lista),znajdz(X+1,Y+1,Lista)];
 sasiedzi(_,_,_,_) -> [].	
-	
+
+%---------------------------------------------------	
 %znajdz Pid dla podanego X i Y w liscie krotek{X,Y,PID}
 znajdz(X,Y,[{Xx,Yy,PID}|_]) when X == Xx, Y == Yy -> PID;
 znajdz(X,Y,[_|T]) -> znajdz(X,Y,T).
 
-%do synchronizacji wymiany stanow
+
+%---------------------------------------------------
+%wysylamy ping do kazdej komorki(synchronizacja)
 ping([]) -> ok;
 ping([{_,_,PID}|T]) -> 
 	PID ! {ping},
 	ping(T).
 
-%odpalamy komorki i tworzymy liste krotek {X,Y, pid} dla kazdej komorki
+%---------------------------------------------------
+%odpalamy komorki i tworzymy liste krotek {X,Y, pid} 
+%dla kazdej komorki
 stworz_kom(X,_,Szer,_,L) when X > Szer -> L;
 stworz_kom(X,Y,Szer,Wys,L) when Y > Wys ->
 	stworz_kom(X+1,1,Szer,Wys,L);
@@ -51,16 +71,36 @@ stworz_kom(X,Y,Szer,Wys,L) ->
 ustaw_stan(PID,Stan) -> PID ! {set, Stan}.
 
 
-%kilka wariantow funkcji ozywiajacej poczatkowe komorki
+%---------------------------------------------------
+%---------------------------------------------------
+%kilka wariantow funkcji ozywiajacej komorki
+%przed startem gry
+%---------------------------------------------------
+%---------------------------------------------------
+
+%<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+%wariant A ->(dla kazdej komorki)wybieramy 
+%jaka jest szansa na ozywienie komorki {1,2,3,...,100}
+%losujemy liczbe z przedzialu od 1 do 100
+%podejmujemy decyzje czy ozywic komorke
 init_poczatkowe_A(_,[]) -> ok;
-init_poczatkowe_A(Rand,[{_,_,PID}|T]) ->
-	decyzja(random:uniform(Rand),PID),
-	init_poczatkowe_A(Rand,T).
+init_poczatkowe_A(Procent,[{_,_,PID}|T]) ->
+	decyzja(Procent,random:uniform(100),PID),
+	init_poczatkowe_A(Procent,T).
 
-decyzja(Rand,PID) when Rand == 1 ->
-	ustaw_stan(PID,zyje);
-decyzja(_,_) -> ok.
+%---------------------------------------------------
+%jesli wylosowana liczba jest mniejsza badz rowna 
+%wybranej przez nas wczesniej szansy to ozywiamy
+%komorke 
+decyzja(Procent,Rand,PID) when Procent < 1 ->
+	decyzja(1,Rand,PID);
+decyzja(Procent,Rand,PID) when Procent > 100 ->
+	decyzja(100,Rand,PID);
+decyzja(Procent,Rand,PID) when Rand =< Procent ->
+	ustaw_stan(PID,zyje).
 
+%<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+%wariant B ->ozywiamy kilka wczesniej wybranych komorek 
 init_poczatkowe_B(Lista) ->
 	znajdz(5,5,Lista) ! {set,zyje},
 	znajdz(6,6,Lista) ! {set,zyje},
@@ -70,3 +110,5 @@ init_poczatkowe_B(Lista) ->
 	znajdz(3,3,Lista) ! {set,zyje},
 	znajdz(3,4,Lista) ! {set,zyje}
 	.
+
+
